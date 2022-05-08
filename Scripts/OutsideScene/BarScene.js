717035500344abb314f3d4f21bd28a9f98d7e123
burabalke <burabalke7776@gmail.com>
2022-05-08 08:50:25 +0200

    Added tiles for BarScene

--- Scripts/OutsideScene/BarScene.js
+++ Scripts/OutsideScene/BarScene.js
@@ -1,7 +1,11 @@
 class BarScene extends OutsideScene{
     requiredAssets = {
         "Shield":  "Sprites/Shield.png",
-        "Chair": "Sprites/Chair.png"
+        "Chair": "Sprites/Chair.png",
+        "Table": "Sprites/Table.png",
+        "Wood1": "Sprites/Tiles/Wood1.png",
+        "Wood2": "Sprites/Tiles/Wood2.png",
+        "Wood3": "Sprites/Tiles/Wood3.png"
 
     }
 
@@ -13,7 +17,17 @@ class BarScene extends OutsideScene{
     load(){
         super.load();
 
-        this.setMapMatrix([[1, 1],[1, 2]]);
+        this.setMapMatrix([[2, 1],[1, 1]]);
+
+        for (let x = 0; x < this.getDimensions().width + 40; x += 40){
+            for (let y = 0; y < this.getDimensions().height + 40; y += 40){
+                let spriteIndex = Math.floor(Math.random() * 3) + 1;
+                this.npcList.push(new Tile(this.backgroundContainer, this.playerReference, {x:x, y:y}, "Wood" + spriteIndex));
+            }
+        }
+
+        this.npcList.push(new Chair(this.container, this.playerReference, {x:100, y:100}, [["YOooo"]]))
+        
 
     }
 
