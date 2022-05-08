bf6068bbc74e801158ebad5680dc488dab24be44
burabalke <burabalke7776@gmail.com>
2022-05-08 12:45:55 +0200

    More features for bar scene

--- Scripts/OutsideScene/BarScene.js
+++ Scripts/OutsideScene/BarScene.js
@@ -10,6 +10,7 @@ class BarScene extends OutsideScene{
 
     }
 
+    tilesList = [];
     constructor(){
         super(0x7d4a28);
     }
@@ -20,24 +21,35 @@ class BarScene extends OutsideScene{
 
         this.setMapMatrix([[1, 1],[1, 2]]);
 
+        if (window.localStorage.getItem('BarPlayerCoords') != null){
+            this.setPlayerPosition(window.localStorage.getItem('BarPlayerCoords'));
+        }
+        
         for (let x = -800; x < this.getDimensions().width - 760; x += 40){
             for (let y = -600; y < this.getDimensions().height - 560; y += 40){
                 let spriteIndex = Math.floor(Math.random() * 3) + 1;
-                this.npcList.push(new Tile(this.backgroundContainer, this.playerReference, {x:x, y:y}, "Wood" + spriteIndex));
+                let newTile = new Tile(this.backgroundContainer, this.playerReference, {x:x, y:y}, "Wood" + spriteIndex);
+                newTile.update(0, inputs);
+                this.tilesList.push(newTile);
             }
         }
 
         this.npcList.push(new Chair(this.container, this.playerReference, {x:100, y:100}, [["YOooo"]]))
         this.npcList.push(new TutorialNpc(this.container, this.playerReference, {x:200, y:300}, [["I'm T"]], 'T', 'Tutorial', []))
-
+        this.npcList.push(new TrapDoor(this.container, this.playerReference, {x:500, y:600}, [['yoo']], 
+        !window.localStorage.getItem('BarPlayerCoords') == null))
+        
     }
 
 
     update(delta, inputs){
         super.update(delta, inputs);
+        
     }
 
+
     destroy(){
+        window.localStorage.setItem('BarPlayerCoords', {x:this.playerReference.x, y:this.playerReference.y});
         super.destroy();
     }
 
